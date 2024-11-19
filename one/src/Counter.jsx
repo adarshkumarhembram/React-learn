d3d36e8fac40d91adb98ef2785b48251da88c18f
adarshkumarhembram <hembramadarshkumar@gmail.com>
2024-11-19 20:00:05 +0530

    LEARN E TARGET VALUE

--- one/src/Counter.jsx
+++ one/src/Counter.jsx
@@ -1,19 +1,15 @@
 import { useState } from "react";
 
+
 function Counter (){
-    const [x,setX] = useState(0);
-    const [y,setY] = useState(0)
-   return(
-    <div>
-        <h1>x: {x}</h1>
-        <h1>y: {y}</h1>
-        <button onClick={()=>setX(x+1)}>change the value of x</button><br />
-        <br />
-        <button onClick={()=>setY(x+1)}>change the value of y</button>
-
-
-    </div>
-   )
+    const [text,setText] = useState("")
+    return(
+        <div>
+            <input type="text" onChange={(event)=>setText(event.target.value)}  />
+            <br />
+            <span>{text}</span>
+        </div>
+    )
 }
 
 export default Counter;
